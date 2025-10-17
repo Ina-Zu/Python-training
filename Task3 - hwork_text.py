a5e7494acda4e36ff7649ad35efffda0c97643d6
Ina-Zu <zumellem@gmail.com>
2025-10-17 14:13:24 +0200

    Commited Task 3 - hwork_text.py

--- Task3 - hwork_text.py
+++ Task3 - hwork_text.py
@@ -1,5 +1,27 @@
 import re
 
+def normalize_text(text):
+    text = re.sub(r'(\b)iZ(\b)', r'\1is\2', text, flags=re.IGNORECASE)
+    text = text.lower().capitalize()
+    return text
+
+def create_summary_sentence(text):
+    sentences = re.split(r'(?<=[.!?])\s+', text.strip())
+    last_words = [re.findall(r'\b\w+\b(?=[.!?])', s)[-1] for s in sentences if re.findall(r'\b\w+\b(?=[.!?])', s)]
+    summary_sentence = ' '.join(last_words).capitalize() + '.'
+    return summary_sentence
+
+def count_whitespaces(text):
+    return len(re.findall(r'\s', text))
+
+def process_text(text):
+    normalized = normalize_text(text)
+    summary_sentence = create_summary_sentence(normalized)
+    final_text = normalized + " " + summary_sentence
+    whitespace_count = count_whitespaces(final_text)
+    print(final_text)
+    print("Number of whitespace characters:", whitespace_count)
+
 text = """
   tHis iz your homeWork, copy these Text to variable.
 
@@ -10,15 +32,5 @@ text = """
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
 """
 
-text = re.sub(r'\biZ\b', 'is', text)
-text = text.lower().capitalize()
+process_text(text)
 
-sentences = re.split(r'(?<=[.!?])\s+', text.strip())
-last_words = [re.findall(r'\b\w+\b(?=[.!?])', s)[-1] for s in sentences if re.findall(r'\b\w+\b(?=[.!?])', s)]
-summary_sentence = ' '.join(last_words).capitalize() + '.'
-text = ' '.join(sentences) + ' ' + summary_sentence
-
-whitespace_count = len(re.findall(r'\s', text))
-
-print(text)
-print("Number of whitespace characters:", whitespace_count)
